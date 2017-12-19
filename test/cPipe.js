0b202d266a11a1462fefbd592b6d923912edb36e
_meugen_ <eugen35@mail.ru>
2017-12-19 18:06:50 +0500

    Подправил тест для cPipe

--- test/cPipe.js
+++ test/cPipe.js
@@ -1,15 +1,19 @@
 import {expect} from 'chai';
-import {cPipeWithFind} from "../src/cPipe";
+import {cPipeWithFind, cPipeWithFor} from "../src/cPipe";
 
 describe("cPipeWithFind", function () {
   const inc = n => ++n;
   const dec = n => --n;
   const double = n => n * 2;
-  it('cPipeWithFind',() => {
-    const piped = cPipeWithFind(res=>4===res)(inc,double,dec);
-    expect(piped(1)).to.equal(4); // breaked
-    expect(piped(2)).to.equal(5); // not breaked
-  });
+
+  [cPipeWithFind, cPipeWithFor].forEach(fn =>
+    it(fn.name,() => {
+      const piped = fn(res=>4===res)(inc,double,dec);
+      expect(piped(1)).to.equal(4); // breaked
+      expect(piped(2)).to.equal(5); // not breaked
+    })
+  );
+
 });
 
 
