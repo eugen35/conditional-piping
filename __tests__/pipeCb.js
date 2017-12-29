5878c654bd654f34814c815fc1b1e688fae23822
_meugen_ <eugen35@mail.ru>
2017-12-29 22:00:34 +0500

    #0.3.0 Added pipeCb, composeCb, cPipeCb, cComposeCb

--- __tests__/pipeCb.js
+++ __tests__/pipeCb.js
@@ -1,14 +1,36 @@
 import {expect} from 'chai';
-import {pipeCb} from "../src/cPipeCb";
+import {pipeCb, composeCb, cPipeCb, cComposeCb} from "../src/cPipeCb";
 
 describe("pipeCb", function () {
   const incCb = (n, cb) => cb(null,++n);
   const doubleCb = (n, cb) => cb(null,n*2);
+  const decCb = (n, cb) => cb(null,--n);
 
   it('pipeCb',done => {
-    const piped = pipeCb(incCb, doubleCb);
-    piped(3,(err,res)=>{
-      expect(res).to.equal(8);
+    const piped = pipeCb(incCb, doubleCb, decCb);
+    piped(1,(err,res)=>{
+      expect(res).to.equal(3);
+      done();
+    });
+  });
+  it('cPipeCb',done => {
+    const piped = cPipeCb(res=>4===res)(incCb, doubleCb, decCb);
+    piped(1,(err,res)=>{
+      expect(res).to.equal(4);
+      done();
+    });
+  });
+  it('composeCb',done => {
+    const composed = composeCb(decCb, doubleCb, incCb);
+    composed(1,(err,res)=>{
+      expect(res).to.equal(3);
+      done();
+    });
+  });
+  it('cComposeCb',done => {
+    const composed = cComposeCb(res=>4===res)(decCb, doubleCb, incCb);
+    composed(1,(err,res)=>{
+      expect(res).to.equal(4);
       done();
     });
   });
--- src/cPipeCb.js
+++ src/cPipeCb.js
@@ -1,17 +1,23 @@
-export const cPipeCb = (breakCondition)=>(...fns)=>(state)=>{
-  for (let i=0; i<fns.length; i++) {
-    state = fns[i](state);
-    if (breakCondition(state)) break;
-  }
-  return state;
+export const pipeCb = (...fns)=>(state, cb)=>{
+  let i=0;
+  const innerCb = (err, state)=> fns.length === ++i ? cb(err, state) : fns[i](state, innerCb);
+  fns[i](state, innerCb);
 };
 
-export const pipeCb = (...fns)=>{
-  const piped = (state, cb) => {
-  if (fns.length === 0) {
-    cb(null, state);
-  } else {
-    fns[i](state, (err, res) => piped(res, ()));
-  }
-}};
+export const cPipeCb = (breakCondition)=>(...fns)=>(state, cb)=>{
+  let i=0;
+  const innerCb = (err, state)=> (breakCondition(state) || fns.length === ++i) ? cb(err, state) : fns[i](state, innerCb);
+  fns[i](state, innerCb);
+};
 
+export const composeCb = (...fns)=>(state, cb)=>{
+  let i=fns.length-1;
+  const innerCb = (err, state)=> --i < 0 ? cb(err, state) : fns[i](state, innerCb);
+  fns[i](state, innerCb);
+};
+
+export const cComposeCb = (breakCondition)=>(...fns)=>(state, cb)=>{
+  let i=fns.length-1;
+  const innerCb = (err, state)=> (breakCondition(state) || --i < 0) ? cb(err, state) : fns[i](state, innerCb);
+  fns[i](state, innerCb);
+};
